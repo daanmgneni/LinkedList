544b1e628405829d404bef3752bb08c2c5625685
DAAN <daanmgneni@gmail.com>
2023-04-01 08:22:07 +0530

    Add:Simple Linkedlist

---
+++ LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int Data)
+        {
+            Node node = new Node(Data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+
+            }
+
+        }
+
+        public void Display()
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+
+
+        }
+
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProgram
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+        public Node(int Data)  //constructor
+        {
+            this.Data = Data;
+        }
+    }
+}
+Footer
+© 2023 GitHub,
