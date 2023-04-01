4d7739a381afc480cf79daa0264c51034964feec
DAAN <daanmgneni@gmail.com>
2023-04-01 08:24:47 +0530

    Add:Check Sequence

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linklist problem!");
+            Console.WriteLine("Welcome to Linkedlist problem!");
+            Console.WriteLine("**********************************");
+
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
